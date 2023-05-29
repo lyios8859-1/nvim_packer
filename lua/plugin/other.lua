e22fa28bc6922d508f5e8ff022ccfc81c4db926e
timly <194962218@qq.com>
2023-05-29 22:14:02 +0800

    Packer -> nvim

--- lua/plugin/other.lua
+++ lua/plugin/other.lua
@@ -5,12 +5,15 @@ require('toggleterm').setup({
   direction = 'float',
   open_mapping = [[<c-\>]]
 })
+
+
 require('ufo').setup({
   promder_selector = function()
-    vim.o.foldcolumn = '1' -- '0' is not bad
+    zim.o.foldcolumn = '1' -- '0' is not bad
     vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
     vim.o.foldlevelstart = 99
-    vim.o.foldenable = true
+    -- 默认展开
+    vim.o.foldenable = false
 
     vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
     vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
